62cdb21dfa54038b784529b78c0eb34d595a2bdb
jp <48197513+daronirl@users.noreply.github.com>
2023-02-21 23:25:39 -0800

    created hidden class

---
+++ app.js
@@ -0,0 +1,55 @@
+const addItem = document.querySelectorAll('.add-button');
+const removeItem = document.querySelector('.remove-button');
+const hidden = document.querySelector('.hidden');
+const itemDisplay = document.querySelector('.order-complete');
+
+const lCoffee =  {
+
+        coffee: 'Large Coffee',
+        price: '$4.00'
+    }
+
+
+// addItem.addEventListener('click', addItemToList);
+
+
+
+// function addItemToList () {
+// hidden.classList.remove('hidden');
+
+
+// }
+
+
+// addItem.forEach(function (btns) {
+//   btns.addEventListener('click', function() {
+//    hidden.classList.remove('hidden');
+
+ 
+
+//   });
+// });
+
+
+
+//  function tassie () { 
+// for (let i = 0; i < addItem.length; i++) {
+//     addItem[i].addEventListener('click', function() {
+//          hidden.classList.remove('hidden');
+//          let html = `
+//          ${lCoffee.coffee} ${lCoffee.price}
+
+//          `;
+
+         
+//          itemDisplay.innerHTML = html;
+         
+
+     
+//     });
+   
+//   } 
+// }
+
+
+// tassie();
