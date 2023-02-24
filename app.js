ae45243d8100ea52eedef1ef9e645597449852a1
jp <48197513+daronirl@users.noreply.github.com>
2023-02-24 01:03:57 -0800

    messed with some functions

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const addItem = document.querySelectorAll('.add-button');
 const removeItem = document.querySelector('.remove-button');
 const hidden = document.querySelector('.hidden');
 const itemDisplay = document.querySelector('.order-complete');
+const btnCoffee = document.querySelector('.btn-coffee');
+const btnEspresso = document.querySelector('#btn-espresso');
+const btnBagel = document.querySelector('#btn-bagel');
+const btnShake = document.querySelector('#btn-shake');
 
 // const lCoffee =  {
 
@@ -9,6 +13,10 @@ const itemDisplay = document.querySelector('.order-complete');
 //         price: '$4.00'
 //     }
 
+
+btnCoffee.addEventListener('click', () => {
+  console.log(btnCoffee);
+});
 
 // addItem.addEventListener('click', addItemToList);
 
@@ -18,79 +26,93 @@ const itemDisplay = document.querySelector('.order-complete');
 // hidden.classList.remove('hidden');
 
 
-// }
+// // }
 
 
-addItem.forEach(function (btns) {
-  btns.addEventListener('click', function() {
-   hidden.classList.remove('hidden');
+// addItem.forEach(function (btns) {
+//   btns.addEventListener('click', function() {
+//    hidden.classList.remove('hidden');
 
  
 
-  });
-});
+//   });
+// });
 
 
 
-//  function tassie () { 
-// for (let i = 0; i < addItem.length; i++) {
-//     addItem[i].addEventListener('click', function() {
-//          hidden.classList.remove('hidden');
-//          let html = `
-//          ${lCoffee.coffee} ${lCoffee.price}
+// //  function tassie () { 
+// // for (let i = 0; i < addItem.length; i++) {
+// //     addItem[i].addEventListener('click', function() {
+// //          hidden.classList.remove('hidden');
+// //          let html = `
+// //          ${lCoffee.coffee} ${lCoffee.price}
 
-//          `;
+// //          `;
 
          
-//          itemDisplay.innerHTML = html;
+// //          itemDisplay.innerHTML = html;
          
 
      
-//     });
+// //     });
    
-//   } 
-// }
+// //   } 
+// // }
 
 
-// tassie();
+// // tassie();
 
-// function createListItem(item) {
+// // function createListItem(item) {
+// //     const p = document.createElement('p');
+// //     p.innerHTML = `${item}
+// //     <span class="price-of-item">$4.00</span> <button class="remove-button">Remove Item</button>
+    
+// //     `
+
+// //     document.querySelector('.order-complete').appendChild(p);
+    
+
+// // }
+
+
+
+// function createNewItem(item) {
+
 //     const p = document.createElement('p');
-//     p.innerHTML = `${item}
-//     <span class="price-of-item">$4.00</span> <button class="remove-button">Remove Item</button>
+//     p.appendChild(document.createTextNode(item));
+//     p.innerText = 'Large Coffee';
     
-//     `
+//     const button = createButton('remove-button');
+//     const span = createSpan('price-of-item');
+   
 
-//     document.querySelector('.order-complete').appendChild(p);
-    
-
-// }
-
-
-
-function createNewItem(item) {
-
-    const p = document.createElement('p');
-    p.appendChild(document.createTextNode(item));
-    p.innerText = 'Large Coffee';
-    const span = document.createElement('span');
-    span.className = 'price-of-item';
-
-
-    const button = document.createElement('button');
-    button.className = 'remove-button';
-    button.innerText = 'Remove Item';
-
-    p.appendChild(span);
-    span.appendChild(button);
+//     p.appendChild(span);
+//     span.appendChild(button);
+   
 
   
     
     
-    document.querySelector('.order-complete').appendChild(p);
-console.log(p.innerHTML);
-}
+//     document.querySelector('.order-complete').appendChild(p);
+// console.log(p.innerHTML);
+// }
 
 
-createNewItem();
+
+// function createButton (classes) {
+//     const button = document.createElement('button');
+//     button.className = classes;
+//     button.innerText = 'Remove Item';
+//     return button;
+// }
+
+// function createSpan (classes) {
+//   const span = document.createElement('span');
+//   span.className = classes;
+//   span.innerText = '$4.00';
+//   return span;
+// }
+
+
+// createNewItem();
 // createListItem('Large Coffee');
