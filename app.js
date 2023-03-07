8e9a9d7437932da056e6cb1a411d02340eb1a98c
John Prescott <48197513+daronirl@users.noreply.github.com>
2023-03-06 17:48:38 -0800

    added a counter to add to each quantity

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const removeItem = document.querySelector('.remove-button');
 const hidden = document.querySelector('.hidden');
 const itemDisplay = document.querySelector('.order-complete');
-const btnCoffee = document.querySelector('.btn-coffee');
-const btnEspresso = document.querySelector('.btn-espresso');
-const btnBagel = document.querySelector('.btn-bagel');
-const btnShake = document.querySelector('.btn-shake');
+const btnCoffee = document.querySelector('#btn-coffee');
+const btnEspresso = document.querySelector('#btn-espresso');
+const btnBagel = document.querySelector('#btn-bagel');
+const btnShake = document.querySelector('#btn-shake');
 const priceOfItem = document.querySelector('.price-of-item');
+
+
 
 const cost = document.querySelector('#cost');
 
@@ -19,79 +21,79 @@ btnShake.addEventListener('click', addMilkShakeOrder);
 
 
 
-const tassieCoffee = 
+const tassieCoffee = [
 
-  {
-
+{
     lgcoffee: 'Large Coffee',
     priceCoffee: '$4.00',
+
+},
+
+{
 
     lgEspresso: 'Espresso',
     priceCoffee: '$5.00',
 
+},
+
+{
+
     lgBagel: 'Bagel',
     priceCoffee: '$2.00',
 
+
+},
+
+{
     lgMilk: 'Milkshake',
     priceCoffee: '$6.00'
-   
-
-  }
-
- 
 
 
+}
+
+]
+
+
+
+
+let quantity = 0; // initialize quantity variable to 0
+let clickCount = 0;
 
 
 // FUNCTIONS TO CREATE ELEMENTS FOR NEW ORDERS
 function addCoffeeOrder () {
  
   hidden.classList.remove('hidden');
-    // const p = document.createElement('p');
-    // p.appendChild(document.createTextNode("Large Coffee:"));
-    // const button = document.createElement('button');
-    // button.className = 'remove-button';
-    // p.appendChild(button);
-
-
-    // document.querySelector('.order-complete').appendChild(p);
-
-   
-    const markup = `
     
-    <p>${tassieCoffee.lgcoffee} - <span class="price-of-item">${tassieCoffee.priceCoffee}</span> <button class="remove-button">Remove Item</button></p>
+
+   quantity++
+   
+   const markup = `
+    
+    <p>${tassieCoffee[0].lgcoffee} - <span class="price-of-item">${tassieCoffee[0].priceCoffee}</span> - ${quantity} <button class="remove-button">Remove </button></p>
     
     `;
 
     document.querySelector('.order-complete').innerHTML = markup;
 
 
-
-  
 
 }
 
 
 function addEspressoOrder () {
-
-  // hidden.classList.remove('hidden');
-  // const p = document.createElement('p');
-  // p.appendChild(document.createTextNode('Espresso'));
-
-  // document.querySelector('.order-complete').appendChild(p);
-
-
     hidden.classList.remove('hidden');
+
+    quantity++
     const markup = `
     
-    <p>${tassieCoffee.lgEspresso} - <span class="price-of-item">${tassieCoffee.priceCoffee}</span> <button class="remove-button">Remove Item</button></p>
+    <p>${tassieCoffee[1].lgEspresso} - <span class="price-of-item">${tassieCoffee[1].priceCoffee}</span> - ${quantity} <button class="remove-button">Remove</button></p>
     
     `;
 
     document.querySelector('.order-complete').innerHTML = markup;
 
-
-  
+ 
   
   
 }
@@ -111,11 +113,11 @@ function addBagelOrder () {
     hidden.classList.remove('hidden');
     const markup = `
     
-    <p>${tassieCoffee.lgBagel} - <span class="price-of-item">${tassieCoffee.priceCoffee}</span> <button class="remove-button">Remove Item</button></p>
+    <p>${tassieCoffee[2].lgBagel} - <span class="price-of-item">${tassieCoffee[2].priceCoffee}</span> <button class="remove-button">Remove</button></p>
     
     `;
 
-    document.querySelector('.order-complete').innerHTML = markup;
+    document.querySelector('.order-complete').innerHTML += markup;
 
 
 
@@ -136,11 +138,11 @@ function addMilkShakeOrder () {
     hidden.classList.remove('hidden');
     const markup = `
     
-    <p>${tassieCoffee.lgMilk} - <span class="price-of-item">${tassieCoffee.priceCoffee}</span> <button class="remove-button">Remove Item</button></p>
+    <p>${tassieCoffee[3].lgMilk} - <span class="price-of-item">${tassieCoffee[3].priceCoffee}</span> <button class="remove-button">Remove</button></p>
     
     `;
 
-    document.querySelector('.order-complete').innerHTML = markup;
+    document.querySelector('.order-complete').innerHTML += markup;
 
 
 
