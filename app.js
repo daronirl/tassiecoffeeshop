b0abd0e6e7faa80a48c3c5dc5db1e25679d83aa1
jp <48197513+daronirl@users.noreply.github.com>
2023-03-22 23:19:24 -0700

    Made it to where you click the add item button and it adds up the total of the selected items

--- app.js
+++ app.js
@@ -7,305 +7,158 @@ const btnEspresso = document.querySelector('#btn-espresso');
 const btnBagel = document.querySelector('#btn-bagel');
 const btnShake = document.querySelector('#btn-shake');
 const priceOfItem = document.querySelector('.price-of-item');
+const totalEl = document.querySelector('.total p');
 
 
 
+let totalPrice = 0;
 
-// Define a template for a coffee order
-// const coffeeOrderTemplate = `
-//   <p>Large Coffee <span class="price"></span> <button class="remove-button">Remove</button></p>
-// `;
-
-// function addCoffeeOrder () {
-//   hidden.classList.remove('hidden');
-  
-//   clickCount++;
-//   if (clickCount === 1) {
-//     btnCoffee.disabled = true;
-//   }
-
-//   // Create a new coffee order element using the template
-//   const coffeeOrderElement = document.createElement('div');
-//   coffeeOrderElement.classList.add('coffee-order');
-//   coffeeOrderElement.innerHTML = coffeeOrderTemplate;
-  
-//   const priceElement = coffeeOrderElement.querySelector('.price');
-//   const removeButton = coffeeOrderElement.querySelector('.remove-button');
-  
-//   priceElement.textContent = '$4.00 - ';
-//   priceElement.dataset.quantity = clickCount;
-  
-//   removeButton.addEventListener('click', function () {
-//     coffeeOrderElement.remove();
-//     clickCount--;
-//     if (clickCount === 0) {
-//       btnCoffee.disabled = false;
-//       hidden.classList.add('hidden');
-//     }
-//   });
-  
-//   document.querySelector('.order-complete').appendChild(coffeeOrderElement);
-// }
-
-
-
-let clickCount = 0;
 // FUNCTIONS TO CREATE ELEMENTS FOR NEW ORDERS
 function addCoffeeOrder () {
+
+
     hidden.classList.remove('hidden');
     
+    // create p element for coffee
+    const largeCoffeeEl = document.createElement('p');
+    const text = document.createTextNode('Large Coffee');
+    largeCoffeeEl.appendChild(text); 
 
 
-   
-  const p = document.createElement('p');
-  const button = document.createElement('button');
-  const span = document.createElement('span');
-  const spanPrice = document.createElement('span');
+    // create span for price of item
+    const priceEl = document.createElement('span');
+    const textPrice = document.createTextNode(' $4.00 - ');
+    priceEl.appendChild(textPrice);
+    priceEl.className = 'price-of-item';
+    largeCoffeeEl.appendChild(priceEl);
 
-  p.appendChild(document.createTextNode('Large Coffee'));
-  span.appendChild(document.createTextNode(' $4.00 - '));
-  button.appendChild(document.createTextNode('Remove'));
+    // using createButton function to create remove button
+    const button = createButton('remove-button');
+    largeCoffeeEl.appendChild(button);
+
+    button.addEventListener('click', () => {
+      largeCoffeeEl.remove(); // Remove the coffee order element from the DOM
+      totalPrice -= 5;
+      totalEl.textContent = `Total: $${totalPrice.toFixed(2)}`;
+    });
   
-  spanPrice.id = 'myQuantity';
-  button.id = 'remove-button';
-  
-  p.appendChild(span);
-  span.appendChild(spanPrice);
-  span.appendChild(button);
-  document.querySelector('.order-complete').appendChild(p);
+   document.querySelector('.order-complete').appendChild(largeCoffeeEl);
 
-  const removeItem = p.querySelector('.remove-button');
-  removeItem.addEventListener('click', function () {
-        p.remove();
-        clickCount--;
-        if (clickCount === 0) {
-          btnCoffee.disabled = false;
-          hidden.classList.add('hidden');
-        }
-      });
-  
+
+   totalPrice += 5; // Add the price of the item to the total
+   totalEl.textContent = `Total: $${totalPrice.toFixed(2)}`;
 }
 
 
 
 
 
-// Define a template for a coffee order
-// const espressoOrderTemplate = `
-//   <p>Espresso <span class="price"></span> <button class="remove-button">Remove</button></p>
-// `;
-
-// function addEspressoOrder () {
-//   hidden.classList.remove('hidden');
-  
-//   clickCount++;
-//   if (clickCount === 1) {
-//     btnEspresso.disabled = true;
-//   }
-
-//   // Create a new coffee order element using the template
-//   const espressoOrderElement = document.createElement('div');
-//   espressoOrderElement.classList.add('espresso-order');
-//   espressoOrderElement.innerHTML = espressoOrderTemplate;
-  
-//   const priceElement = espressoOrderElement.querySelector('.price');
-//   const removeButton = espressoOrderElement.querySelector('.remove-button');
-  
-//   priceElement.textContent = '$4.00 - ';
-//   priceElement.dataset.quantity = clickCount;
-  
-//   removeButton.addEventListener('click', function () {
-//     espressoOrderElement.remove();
-//     clickCount--;
-//     if (clickCount === 0) {
-//       btnEspresso.disabled = false;
-//       hidden.classList.add('hidden');
-//     }
-//   });
-  
-//   document.querySelector('.order-complete').appendChild(espressoOrderElement);
-// }
-
  
 function addEspressoOrder () {
   hidden.classList.remove('hidden');
-    
+
+    // create p element for espresso
+    const espressoEl = document.createElement('p');
+    const text = document.createTextNode('Espresso');
+    espressoEl.appendChild(text); 
 
 
-const p = document.createElement('p');
-const button = document.createElement('button');
-const span = document.createElement('span');
-const spanPrice = document.createElement('span');
+    // create span for price of item
+    const priceEl = document.createElement('span');
+    const textPrice = document.createTextNode(' $5.00 - ');
+    priceEl.appendChild(textPrice);
+    priceEl.className = 'price-of-item';
+    espressoEl.appendChild(priceEl);
 
-p.appendChild(document.createTextNode('Espresso'));
-span.appendChild(document.createTextNode(' $5.00 - '));
-button.appendChild(document.createTextNode('Remove'));
+    // using createButton function to create remove button
+    const button = createButton('remove-button');
+    espressoEl.appendChild(button);
 
-spanPrice.id = 'myQuantity';
-button.id = 'remove-button';
-
-p.appendChild(span);
-span.appendChild(spanPrice);
-span.appendChild(button);
-document.querySelector('.order-complete').appendChild(p);
-
-
-
-
-
- 
+    button.addEventListener('click', () => {
+      espressoEl.remove(); // Remove the coffee order element from the DOM
+    });
   
-  
+   document.querySelector('.order-complete').appendChild(espressoEl);
+
+
+   
 }
 
 
 function addBagelOrder () {
   hidden.classList.remove('hidden');
     
-  clickCount++
-  if  (clickCount === 5) {
-      btnBagel.disabled = true;
-  }
+  // create p element for coffee
+  const bagelEl = document.createElement('p');
+  const text = document.createTextNode('Bagel');
+  bagelEl.appendChild(text); 
 
 
-quantity++
-if (quantity === 1) {
-const p = document.createElement('p');
-const button = document.createElement('button');
-const span = document.createElement('span');
-const spanPrice = document.createElement('span');
+  // create span for price of item
+  const priceEl = document.createElement('span');
+  const textPrice = document.createTextNode(' $2.00 - ');
+  priceEl.appendChild(textPrice);
+  priceEl.className = 'price-of-item';
+  bagelEl.appendChild(priceEl);
 
-p.appendChild(document.createTextNode('Bagel'));
-span.appendChild(document.createTextNode(' $2.00 - '));
-button.appendChild(document.createTextNode('Remove'));
+  // using createButton function to create remove button
+  const button = createButton('remove-button');
+  bagelEl.appendChild(button);
 
-spanPrice.id = 'myQuantity';
-button.id = 'remove-button';
+  button.addEventListener('click', () => {
+    bagelEl.remove(); // Remove the coffee order element from the DOM
+  });
 
-p.appendChild(span);
-span.appendChild(spanPrice);
-span.appendChild(button);
-document.querySelector('.order-complete').appendChild(p);
+ document.querySelector('.order-complete').appendChild(bagelEl);
 
 
-} else {
-let spanPrice = document.querySelector('#myQuantity');
-spanPrice.textContent = quantity;
-
-}
-
-
+ 
  
 }
 
 
 function addMilkShakeOrder () {
   hidden.classList.remove('hidden');
-    
-  clickCount++
-  if  (clickCount === 5) {
-      btnShake.disabled = true;
-  }
+
+    // create p element for coffee
+    const shakeEl = document.createElement('p');
+    const text = document.createTextNode('Milkshakes');
+    shakeEl.appendChild(text); 
 
 
-quantity++
-if (quantity === 1) {
-const p = document.createElement('p');
-const button = document.createElement('button');
-const span = document.createElement('span');
-const spanPrice = document.createElement('span');
+    // create span for price of item
+    const priceEl = document.createElement('span');
+    const textPrice = document.createTextNode(' $4.00 - ');
+    priceEl.appendChild(textPrice);
+    priceEl.className = 'price-of-item';
+    shakeEl.appendChild(priceEl);
 
-p.appendChild(document.createTextNode('Milkshake'));
-span.appendChild(document.createTextNode(' $6.00 - '));
-button.appendChild(document.createTextNode('Remove'));
+    // using createButton function to create remove button
+    const button = createButton('remove-button');
+    shakeEl.appendChild(button);
 
-spanPrice.id = 'myQuantity';
-button.id = 'remove-button';
-
-p.appendChild(span);
-span.appendChild(spanPrice);
-span.appendChild(button);
-document.querySelector('.order-complete').appendChild(p);
-
-
-} else {
-let spanPrice = document.querySelector('#myQuantity');
-spanPrice.textContent = quantity;
-
-}
-
-
-
-
+    button.addEventListener('click', () => {
+      shakeEl.remove(); // Remove the coffee order element from the DOM
+    });
   
+   document.querySelector('.order-complete').appendChild(shakeEl);
+
+
+   
+    
+
  
 }
 
+// create remove button 
+function createButton (classes) {
+  const button = document.createElement('button');
+  const textRemoveButton = document.createTextNode('Remove');
+  button.appendChild(textRemoveButton);
+  button.className = classes;
+  return button;
 
-
-
-
-
-// addItem.addEventListener('click', addItemToList);
-
-
-
-// function addItemToList () {
-// hidden.classList.remove('hidden');
-
-
-// // }
-
-
-// addItem.forEach(function (btns) {
-//   btns.addEventListener('click', function() {
-//    hidden.classList.remove('hidden');
-
- 
-
-//   });
-// });
-
-// function createNewItem(item) {
-
-//     const p = document.createElement('p');
-//     p.appendChild(document.createTextNode(item));
-//     p.innerText = 'Large Coffee';
-    
-//     const button = createButton('remove-button');
-//     const span = createSpan('price-of-item');
-   
-
-//     p.appendChild(span);
-//     span.appendChild(button);
-   
-
-  
-    
-    
-//     document.querySelector('.order-complete').appendChild(p);
-// console.log(p.innerHTML);
-// }
-
-
-
-// function createButton (classes) {
-//     const button = document.createElement('button');
-//     button.className = classes;
-//     button.innerText = 'Remove Item';
-//     return button;
-// }
-
-// function createSpan (classes) {
-//   const span = document.createElement('span');
-//   span.className = classes;
-//   span.innerText = '$4.00';
-//   return span;
-// }
-
-
-// createNewItem();
-// createListItem('Large Coffee');
+}
 
 
 
